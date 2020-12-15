3c26f73321bd01f4b457ca55830f275fb8d78302
ajid2 <ajidalmajid6@gmail.com>
2020-12-16 05:41:49 +0700

    update chat konsumen

--- application/views/konsumen/chat/index.php
+++ application/views/konsumen/chat/index.php
@@ -6,20 +6,20 @@
                 <div class="col-xl-12 col-lg-12 col-md-12">
 
                     <div class="chat-system">
-                        <div class="hamburger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu mail-menu d-lg-none">
+                        <!-- <div class="hamburger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu mail-menu d-lg-none">
                                 <line x1="3" y1="12" x2="21" y2="12"></line>
                                 <line x1="3" y1="6" x2="21" y2="6"></line>
                                 <line x1="3" y1="18" x2="21" y2="18"></line>
-                            </svg></div>
-                        <div class="user-list-box">
-                            <div class="search">
+                            </svg></div> -->
+                        <!-- <div class="user-list-box"> -->
+                        <!-- <div class="search">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search">
                                     <circle cx="11" cy="11" r="8"></circle>
                                     <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
                                 </svg>
                                 <input type="text" class="form-control" placeholder="Search" />
-                            </div>
-                            <div class="people">
+                            </div> -->
+                        <!-- <div class="people">
 
                                 <div class="person" data-chat="person6">
                                     <div class="user-info">
@@ -50,9 +50,9 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="chat-box" style="height: calc(100vh - 250px);">
+                            </div> -->
+                        <!-- </div> -->
+                        <div class="chat-box" style="height: calc(100vh - 195px);border-radius:6px">
 
                             <!-- <div class="chat-not-selected">
                                 <p> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square">
@@ -153,7 +153,7 @@
                             </div> -->
 
                             <div class="chat-box-inner" style="height: 100%;">
-                                <div class="chat-meta-user chat-active">
+                                <!-- <div class="chat-meta-user chat-active">
                                     <div class="current-chat-user-name"><span><img src="assets/img/90x90.jpg" alt="dynamic-image"><span class="name"></span></span></div>
 
                                     <div class="chat-action-btn align-self-center">
@@ -202,8 +202,8 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="chat-conversation-box">
+                                </div> -->
+                                <div class="chat-conversation-box" style="height: calc(100% - 59px);">
                                     <div id="chat-conversation-box-scroll" class="chat-conversation-box-scroll">
                                         <div class="chat active-chat" data-chat="person1">
                                             <div class="conversation-start">
@@ -256,6 +256,7 @@
                 url: "<?= base_url() ?>/konsumen/chat/store",
                 data: $(this).serialize(),
                 success: function(data) {
+                    $('.chat.active-chat').append('<div class="bubble me">' + $('input[name="pesan"]').val() + '</div>');
                     $('input[name="pesan"]').val('');
                 }
             })
